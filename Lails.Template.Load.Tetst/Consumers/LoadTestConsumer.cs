2670a833f2e5c941f40e80b13c1cdac0c16b869c
Sergey Lebedev <limpi11@mail.ru>
2023-09-25 16:47:08 +0700

    TargetFramework net6.0

--- Lails.Template.Load.Tetst/Consumers/LoadTestConsumer.cs
+++ Lails.Template.Load.Tetst/Consumers/LoadTestConsumer.cs
@@ -1,47 +1,47 @@
-﻿using Lails.DBContext;
-using Lails.Template.Load.Tetst.BusinessLogic.Commands;
-using Lails.Template.Load.Tetst.BusinessLogic.Queries;
-using Lails.Transmitter.CrudBuilder;
-using MassTransit;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using Lails.DBContext;
+//using Lails.Template.Load.Tetst.BusinessLogic.Commands;
+//using Lails.Template.Load.Tetst.BusinessLogic.Queries;
+//using Lails.Transmitter.CrudBuilder;
+//using MassTransit;
+//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Lails.Template.Load.Tetst.Consumers
-{
-	public class LoadTestConsumer : IConsumer<ILoadTestEvent>
-	{
-		readonly ICrudBuilder<LailsDbContext> _crudBuilder;
-		readonly LailsDbContext _lailsDbContext;
-		public LoadTestConsumer(ICrudBuilder<LailsDbContext> cRUDBuilder, LailsDbContext lailsDbContext)
-		{
-			_crudBuilder = cRUDBuilder;
-			_lailsDbContext = lailsDbContext;
-		}
-		public async Task Consume(ConsumeContext<ILoadTestEvent> context)
-		{
-			var customer = new Customer
-			{
-				FirstName = "Elizabeth",
-				LastName = "Lincoln",
-				Address = "23 Tsawassen Blvd.",
-				Invoices = new List<Invoice> { new Invoice { Date = DateTime.UtcNow } }
-			};
+//namespace Lails.Template.Load.Tetst.Consumers
+//{
+//	public class LoadTestConsumer : IConsumer<ILoadTestEvent>
+//	{
+//		readonly ICrudBuilder<LailsDbContext> _crudBuilder;
+//		readonly LailsDbContext _lailsDbContext;
+//		public LoadTestConsumer(ICrudBuilder<LailsDbContext> cRUDBuilder, LailsDbContext lailsDbContext)
+//		{
+//			_crudBuilder = cRUDBuilder;
+//			_lailsDbContext = lailsDbContext;
+//		}
+//		public async Task Consume(ConsumeContext<ILoadTestEvent> context)
+//		{
+//			var customer = new Customer
+//			{
+//				FirstName = "Elizabeth",
+//				LastName = "Lincoln",
+//				Address = "23 Tsawassen Blvd.",
+//				Invoices = new List<Invoice> { new Invoice { Date = DateTime.UtcNow } }
+//			};
 
-			await _crudBuilder.Build<CustomerCreate>().Execute(customer);
-
-
-			CustomerFilter filter = new CustomerFilter { Id = customer.Id };
-			var r = await _crudBuilder.Build<CustomerQuery>().ApplyFilter(filter);
+//			await _crudBuilder.Build<CustomerCreate>().Execute(customer);
 
 
-			var r2 = await _lailsDbContext.Set<Customer>().AsQueryable().Where(r => r.Id == customer.Id).AsTracking().ToListAsync();
+//			CustomerFilter filter = new CustomerFilter { Id = customer.Id };
+//			var r = await _crudBuilder.Build<CustomerQuery>().ApplyFilter(filter);
 
-			//Console.WriteLine(customer.Id);
-		}
-	}
 
-	public interface ILoadTestEvent { }
-}
+//			var r2 = await _lailsDbContext.Set<Customer>().AsQueryable().Where(r => r.Id == customer.Id).AsTracking().ToListAsync();
+
+//			//Console.WriteLine(customer.Id);
+//		}
+//	}
+
+//	public interface ILoadTestEvent { }
+//}
--- Lails.Template.Load.Tetst/Controllers/StartController.cs
+++ Lails.Template.Load.Tetst/Controllers/StartController.cs
@@ -1,30 +1,30 @@
-﻿using Lails.Template.Load.Tetst.Consumers;
-using Microsoft.AspNetCore.Mvc;
-using NetCoreDataBus;
-using System.Threading.Tasks;
+﻿//using Lails.Template.Load.Tetst.Consumers;
+//using Microsoft.AspNetCore.Mvc;
+//using NetCoreDataBus;
+//using System.Threading.Tasks;
 
-namespace Lails.Template.Load.Tetst.Controllers
-{
-	[Route("api/test")]
-	[ApiController]
-	public class StartController : ControllerBase
-	{
-		readonly IBusPublisher _busPublisher;
-		public StartController(IBusPublisher busPublisher)
-		{
-			_busPublisher = busPublisher;
-		}
+//namespace Lails.Template.Load.Tetst.Controllers
+//{
+//	[Route("api/test")]
+//	[ApiController]
+//	public class StartController : ControllerBase
+//	{
+//		readonly IBusPublisher _busPublisher;
+//		public StartController(IBusPublisher busPublisher)
+//		{
+//			_busPublisher = busPublisher;
+//		}
 
-		[Route("startLoadTest")]
-		[HttpGet]
-		public async Task StartLoadTest()
-		{
-			for (int i = 0; i < 90000; i++)
-			{
-				await _busPublisher.PublishAsync(new LoadTestEvent());
-			}
-		}
+//		[Route("startLoadTest")]
+//		[HttpGet]
+//		public async Task StartLoadTest()
+//		{
+//			for (int i = 0; i < 90000; i++)
+//			{
+//				await _busPublisher.PublishAsync(new LoadTestEvent());
+//			}
+//		}
 
-		public class LoadTestEvent : ILoadTestEvent { }
-	}
-}
+//		public class LoadTestEvent : ILoadTestEvent { }
+//	}
+//}
